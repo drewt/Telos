88e2694d21ead49d42d04c3c919523a0d0460ce7
Drew Thoreson <drew.thoreson@alumni.ubc.ca>
2013-01-27 11:26:03 -0800

    Improve sleep_test()

--- src/usr/eventtest.c
+++ src/usr/eventtest.c
@@ -1,10 +1,28 @@
+/*  Copyright 2013 Drew T.
+ *
+ *  This file is part of Telos.
+ *  
+ *  Telos is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  Telos is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Telos.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <telos/process.h>
 #include <telos/print.h>
 #include <signal.h>
 
 static void sleep_proc (int argc, char *argv[]) {
     syssleep (argc);
-    sysreport ("");
+    printf ("%d ", argc);
 }
 
 static void sleep_test (void) {
@@ -14,7 +32,7 @@ static void sleep_test (void) {
     syscreate (sleep_proc, 1440, NULL);
     syssleep (2000);
 
-    printf ("\nVefify descending: ");
+    printf ("\nVefify ascending: ");
     syscreate (sleep_proc, 1440, NULL);
     syscreate (sleep_proc, 990, NULL);
     syscreate (sleep_proc, 100, NULL);
