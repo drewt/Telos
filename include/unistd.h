092f0e7f1909f77913b9e34243dd9a557cf978ff
Drew Thoreson <drew.thoreson@alumni.ubc.ca>
2013-01-28 19:41:13 -0800

    Move things into unistd.h and klib/unistd.c
    syssleep() is renamed sleep(), and its resolution is increased to
    seconds (from milliseconds) to comply with standard UNIX sleep function.
    sleep(), alarm() and getpid() definitions have been moved into
    klib/unistd.c, with declarations in <unistd.h>.
    
    Other minor changes:
        * <telos/filedes.h> incorporated into <unistd.h> and removed
        * <mem.h> moved to <kernel/mem.h> since it's really a kernel header
        * `size' parameter to kmalloc()/hmalloc() changed to unsigned int

---
+++ include/unistd.h
@@ -0,0 +1,40 @@
+/*  Copyright 2013 Drew T.
+ *
+ *  This file is part of the Telos C Library.
+ *  
+ *  Telos is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  Telos is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Telos.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __UNISTD_H_
+#define __UNISTD_H_
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+typedef unsigned long size_t;
+typedef long ssize_t;
+typedef int pid_t;
+
+enum std_fds {
+    STDIN_FILENO,
+    STDOUT_FILENO,
+    STDERR_FILENO,
+};
+
+unsigned int alarm (unsigned int seconds);
+pid_t getpid (void);
+unsigned int sleep (unsigned int seconds);
+
+#endif // __UNISTD_H_
---
+++ lib/klib/unistd.c
@@ -0,0 +1,32 @@
+/*  Copyright 2013 Drew T.
+ *
+ *  This file is part of the Telos C Library.
+ *  
+ *  Telos is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  Telos is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Telos.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <unistd.h>
+#include <syscall.h>
+
+unsigned int alarm (unsigned int seconds) {
+    return syscall1 (SYS_ALARM, (void*) seconds);
+}
+
+unsigned int sleep (unsigned int seconds) {
+    return syscall1 (SYS_SLEEP, (void*) seconds);
+}
+
+pid_t getpid (void) {
+    return syscall0 (SYS_GETPID);
+}
