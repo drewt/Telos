27d3e5d15b44065b01cb1e8815e4bb99b2291aef
Drew Thoreson <drew.thoreson@alumni.ubc.ca>
2013-02-02 12:58:08 -0800

    Move <signal.h> functions into klib.a

--- lib/klib/signal.c
+++ lib/klib/signal.c
@@ -1,9 +1,6 @@
-/* syscall.c : system calls
- */
-
 /*  Copyright 2013 Drew T.
  *
- *  This file is part of Telos.
+ *  This file is part of the Telos C Library.
  *  
  *  Telos is free software: you can redistribute it and/or modify it under the
  *  terms of the GNU General Public License as published by the Free Software
@@ -19,74 +16,43 @@
  *  with Telos.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stddef.h>
-#include <unistd.h>
+#include <syscall.h>
 #include <signal.h>
 
-#include <syscall.h>
-#include <telos/process.h>
-#include <telos/msg.h>
-
-typedef int pid_t;
-
-/* PROCESS.H */
-
 /*-----------------------------------------------------------------------------
- * */
+ * Registers an action for a given signal */
 //-----------------------------------------------------------------------------
-pid_t syscreate (void(*func)(int,char**), int argc, char *argv[]) {
-    return syscall3 (SYS_CREATE, func, (void*) argc, argv);
+int sigaction (int sig, struct sigaction *act, struct sigaction *oact) {
+    return syscall3 (SYS_SIGACTION, (void*) sig, act, oact);
 }
 
 /*-----------------------------------------------------------------------------
- * */
+ * Sets the signal handler for a given signal */
 //-----------------------------------------------------------------------------
-void sysyield (void) {
-    syscall0 (SYS_YIELD);
+void (*signal(int sig, void (*func)(int)))(int) {
+    return (void(*)(int)) syscall2 (SYS_SIGNAL, (void*) sig, func);
 }
 
 /*-----------------------------------------------------------------------------
- * */
+ * Alters the signal mask, either by blocking the given signals, unblocking
+ * them, or setting the signal mask to the given set */
 //-----------------------------------------------------------------------------
-void sysstop (void) {
-    syscall0 (SYS_STOP);
-}
-
-/* PRINT.H */
-
-/*-----------------------------------------------------------------------------
- * */
-//-----------------------------------------------------------------------------
-void sysreport (char *s) {
-    syscall1 (SYS_REPORT, s);
-}
-
-/* MSG.H */
-
-/*-----------------------------------------------------------------------------
- * */ 
-//-----------------------------------------------------------------------------
-int send (pid_t pid, void *obuf, int olen, void *ibuf, int ilen) {
-    return syscall5 (SYS_SEND, (void*) pid, obuf, (void*) olen, ibuf,
-            (void*) ilen);
+int sigprocmask (int how, sigset_t *set, sigset_t *oset) {
+    return syscall3 (SYS_SIGMASK, (void*) how, set, oset);
 }
 
 /*-----------------------------------------------------------------------------
- * */
+ * Sends a signal to a process */
 //-----------------------------------------------------------------------------
-int recv (pid_t *pid, void *buffer, int length) {
-    return syscall3 (SYS_RECV, (void*) pid, buffer, (void*) length);
+int kill (pid_t pid, int signal_number) {
+    return syscall2 (SYS_KILL, (void*) pid, (void*) signal_number);
 }
 
 /*-----------------------------------------------------------------------------
- * */
+ * Suspends execution of the calling process until it receives a signal */
 //-----------------------------------------------------------------------------
-int reply (pid_t pid, void *buffer, int length) {
-    return syscall3 (SYS_REPLY, (void*) pid, buffer, (void*) length);
+int sigwait (void) {
+    return syscall0 (SYS_SIGWAIT);
 }
-
-/* SIGNAL.H */
-
-/* IO.H */
 
 
