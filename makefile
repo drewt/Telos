9a674fa304e1ec5ed08275be9fd0336f1e517c57
Drew Thoreson <drew.thoreson@alumni.ubc.ca>
2013-11-21 19:53:06 -0800

    kbuild-like build system
    Featuring pretty output a-la kbuild (turn off with 'make verbose=y'),
    hidden dependency files, simpler submakes.  The usr/ submake in
    particular is improved, with the printserver.o code duplication
    eliminated using a handy macro.

---
+++ makefile
@@ -0,0 +1,81 @@
+MAKEFLAGS += -rR --no-print-directory
+
+topdir = $(CURDIR)
+incdir = $(topdir)/include
+export topdir incdir
+
+CCPREFIX  = #i586-elf-
+AR        = $(CCPREFIX)ar
+ARFLAGS   = rcs
+AS        = $(CCPREFIX)as --32
+CC        = $(CCPREFIX)gcc -m32 -march=i386
+CFLAGS    = -Wall -Wextra -Wno-unused-parameter
+ALLCFLAGS = -I $(incdir) -fno-builtin -ffreestanding -std=gnu99
+LD        = $(CCPREFIX)ld -m elf_i386
+OBJCOPY   = $(CCPREFIX)objcopy
+
+export CCPREFIX AR ARFLAGS AS CC CFLAGS ALLCFLAGS LD OBJCOPY
+
+findobj = find kernel dispatch drivers -name '*.o' | tr '\n' ' '
+
+submakes = dispatch drivers kernel usr
+clean = kernel.img kernel.bin boot/loader.o
+distclean = pad
+
+all: kernel.img
+
+include rules.mk
+
+do_submake = +@$(if $(quiet),echo '  MAKE    $@' &&) cd $@ && $(MAKE)
+
+quiet_cmd_ldgen  = GEN     linker.ld
+      cmd_ldgen  = printf "INPUT ( %s )\n" \
+		   "boot/loader.o `$(findobj)` usr/usr.a lib/klib.a" \
+		   > linker.ld && cat sections.ld >> linker.ld
+
+quiet_cmd_padgen = GEN     $@
+      cmd_padgen = dd if=/dev/zero of=$@ bs=1 count=750 &> /dev/null
+
+dispatch:
+	$(do_submake)
+
+drivers:
+	$(do_submake)
+
+kernel:
+	$(do_submake)
+
+usr:
+	$(do_submake)
+
+boot/loader.o: boot/loader.s
+	$(call cmd,as)
+
+linker.ld: $(submakes) boot/loader.o
+	$(call cmd,ldgen)
+
+kernel.bin: linker.ld
+	$(call cmd,sld,linker.ld)
+
+pad:
+	$(call cmd,padgen)
+
+# make a bootable floppy image with grub legacy
+kernel.img: boot/stage1 boot/stage2 pad kernel.bin
+	$(call cmd,cat)
+
+clean: topclean
+topclean:
+	@cd dispatch && $(MAKE) clean
+	@cd drivers && $(MAKE) clean
+	@cd kernel && $(MAKE) clean
+	@cd usr && $(MAKE) clean
+
+distclean: topdistclean
+topdistclean:
+	@cd dispatch && $(MAKE) distclean
+	@cd drivers && $(MAKE) distclean
+	@cd kernel && $(MAKE) distclean
+	@cd usr && $(MAKE) distclean
+
+.PHONY: $(submakes) linker.ld topclean topdistclean
---
+++ rules.mk
@@ -0,0 +1,79 @@
+#
+# rules.mk: build rules
+#
+#  For automatic dependency resolution, set $(objects) to the list of object
+#  files needed for the build before including this file.
+#
+#  $(clean) and $(distclean) should be set to the files to be removed on
+#  'make clean' and 'make distclean', respectively.
+#
+#  $(verbose) controls whether build commands are echoed verbatim, or in
+#  prettier "  PROG    FILE" format.  Set verbose=y for verbatim output.
+#
+
+.SUFFIXES:
+.PHONY: clean distclean
+
+dependencies = $(addprefix ., $(addsuffix .d, $(basename $(objects))))
+distclean += $(dependencies)
+
+ifeq ($(verbose),y)
+  quiet =
+else
+  quiet = quiet_
+endif
+
+clean:
+	$(if $(clean), rm -f $(clean))
+
+distclean: clean
+	$(if $(distclean), rm -f $(distclean))
+
+%.o: %.c
+	$(call cmd,cc)
+
+.%.d: %.c
+	$(call cmd,dep)
+
+# CC for program object files (.o)
+quiet_cmd_cc    = CC      $@
+      cmd_cc    = $(CC) -c $(CPPFLAGS) $(ALLCFLAGS) -o $@ $<
+
+# AS for program object files (.o)
+quiet_cmd_as    = AS      $@
+      cmd_as    = $(AS) $(ASFLAGS) -o $@ $<
+
+# create archive
+quiet_cmd_ar    = AR      $@
+      cmd_ar    = $(AR) $(ARFLAGS) $@ $^
+
+# LD for programs; optional parameter: libraries
+quiet_cmd_ld    = LD      $@
+      cmd_ld    = $(LD) $(LDFLAGS) -o $@ $^ $(1)
+
+# scripted LD
+quiet_cmd_sld   = LD      $@
+      cmd_sld   = $(LD) -T $(1) $(LDFLAGS) -o $@
+
+# generate dependencies file
+quiet_cmd_dep   = DEP     $@
+      cmd_dep   = echo "$@ `$(CC) -MM -I $(incdir) $(CPPFLAGS) $<`" > $@
+
+# call submake
+quiet_cmd_smake = MAKE    $@
+      cmd_smake = cd $@ && $(MAKE)
+
+# concatenate prerequisites
+quiet_cmd_cat    = GEN     $@
+      cmd_cat    = cat $^ > $@
+
+# generate man page as plain text file
+quiet_cmd_groff = GROFF   $@
+      cmd_groff = groff -man -Tascii $< | col -bx > $@
+
+# cmd macro (taken from kbuild)
+cmd = @$(if $($(quiet)cmd_$(1)),echo '  $(call $(quiet)cmd_$(1),$(2))' &&) $(call cmd_$(1),$(2))
+
+ifneq ($(dependencies),)
+-include $(dependencies)
+endif
