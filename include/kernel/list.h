ec1532824d77dbb51ee97ee1b6a8b1e36e51ff1e
Drew Thoreson <drew.thoreson@alumni.ubc.ca>
2013-11-23 14:57:21 -0800

    Fix container_of macro
    offsetof needs to be redefined for some reason.  Apparently it was
    returning a pointer.

--- include/kernel/list.h
+++ include/kernel/list.h
@@ -21,6 +21,8 @@
 #include <stddef.h> /* NULL */
 
 /* XXX: GNU */
+#undef offsetof
+#define offsetof __builtin_offsetof
 #define container_of(ptr,type,member) ({				\
 		const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
 		(type *)( (char *)__mptr - offsetof(type,member) );})
