85e7dae324c20cba646cddc98716bd21068aca68
Drew Thoreson <drew.thoreson@alumni.ubc.ca>
2013-12-24 13:04:02 -0800

    interrupts: use asm at file scope for handlers

--- kernel/inthandlers.c
+++ kernel/inthandlers.c
@@ -133,17 +133,22 @@ void exn_err(unsigned int num, struct gp_regs reg)
 	print_cpu_context(current->esp);
 }
 
-#define MAKE_HANDLER(name,x)		\
-static void name(void) {		\
-	asm volatile(			\
+asm("\n"
+"_hlt:"
+	"hlt		\n"
+	"jmp _hlt	\n"
+);
+
+#define MAKE_HANDLER(name, x)		\
+void name(void);			\
+asm("\n"				\
+#name ":"				\
 	"pusha			\n"	\
 	"pushl $"x"		\n"	\
 	"call  exn_err		\n"	\
-	"_hlt"x": "			\
-		"hlt		\n"	\
-		"jmp _hlt"x"	\n"	\
-	);				\
-}
+	"jmp _hlt		\n"	\
+);
+
 
 MAKE_HANDLER(int0, "0")
 MAKE_HANDLER(int1, "1")
