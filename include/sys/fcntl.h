3474e5409ef346417739b2ddbae70eb4a441f721
Drew Thoreson <drew.thoreson@alumni.ubc.ca>
2015-05-26 16:28:22 -0700

    Round out <sys/fcntl.h>
    Add all the constants required by POSIX, and also the struct flock
    definition.

--- include/sys/fcntl.h
+++ include/sys/fcntl.h
@@ -18,6 +18,22 @@
 #ifndef _SYS_FCNTL_H_
 #define _SYS_FCNTL_H_
 
+#include <sys/stat.h> /* file modes */
+#include <sys/type_macros.h>
+
+#ifndef _MODE_T_DEFINED
+#define _MODE_T_DEFINED
+typedef _MODE_T_TYPE mode_t;
+#endif
+#ifndef _OFF_T_DEFINED
+#define _OFF_T_DEFINED
+typedef _OFF_T_TYPE off_t;
+#endif
+#ifndef _PID_T_DEFINED
+#define _PID_T_DEFINED
+typedef _PID_T_TYPE pid_t;
+#endif
+
 #define F_DUPFD         1
 #define F_DUP2FD        2
 #define F_DUPFD_CLOEXEC 3
@@ -55,6 +71,39 @@
 #define O_SYNC      0x4000
 #define O_TRUNC     (0x8000 | O_WRONLY)
 #define O_TTY_INIT  0x10000
+
+#define O_ACCMODE (O_RDONLY | O_WRONLY | O_RDWR)
+
+/* fd for *at functions */
+#define AT_FDWCD (-1)
+
+/* flags for faccessat */
+#define AT_EACCESS 1
+
+/* flags for fstatat, fchmodat, fchownat, utimensat */
+#define AT_SYMLINK_NOFOLLOW 1
+
+/* flags for linkat */
+#define AT_SYMLINK_FOLLOW 1
+
+/* flags for unlinkat */
+#define AT_REMOVEDIR 1
+
+/* advice for posix_fadvise */
+#define POSIX_FADV_DONTNEED   1
+#define POSIX_FADV_NOREUSE    2
+#define POSIX_FADV_NORMAL     3
+#define POSIX_FADV_RANDOM     4
+#define POSIX_FADV_SEQUENTIAL 5
+#define POSIX_FADV_WILLNEED   6
+
+struct flock {
+	short l_type;
+	short l_whence;
+	off_t l_start;
+	off_t l_len;
+	pid_t l_pid;
+};
 
 #ifdef __KERNEL__
 #define O_READ  O_RDONLY
